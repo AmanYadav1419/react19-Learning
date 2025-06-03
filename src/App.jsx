71e83825f93eb9e9aa7d70a63d468d7bcb554dc9
unknown <amanyadav16544@gmail.com>
2025-06-03 22:06:01 +0530

    completed first task of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Greet from "./components/Greet";
 
 const App = () => {
   return (
-    <div>Hello from aman</div>
-  )
-}
+    <div>
+      {/* importing Greet component into App.jsx file */}
+      <Greet />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Greet.jsx
@@ -0,0 +1,6 @@
+// first task completed with the third task that is refactor the code in arrow functions
+const Greet = () => {
+  return <h1>Hello From Greet</h1>;
+};
+
+export default Greet;
