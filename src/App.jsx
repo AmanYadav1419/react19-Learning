47880b566bb6e7a7214bf223a0b316f19886b2c3
unknown <amanyadav16544@gmail.com>
2025-06-04 16:28:36 +0530

    completed basic challenges about components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Greet from "./components/Greet";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       {/* importing Greet component into App.jsx file */}
       <Greet />
+
+      {/* importing all the components for the Excercise task, in an correct order */}
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 };
