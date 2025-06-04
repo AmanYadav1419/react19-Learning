1450caf1c52b032d046e92fb5a031fb60ed2b5b0
unknown <amanyadav16544@gmail.com>
2025-06-04 16:54:48 +0530

    started topic jsx, completed first task/Excercise

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import Greet from "./components/Greet";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
   return (
     <div>
-      {/* importing Greet component into App.jsx file */}
+      {/* importing Greet component for the Component Excercise task */}
       <Greet />
 
       {/* importing all the components for the Excercise task, in an correct order */}
       <Header />
       <MainContent />
       <Footer />
+
+      {/* importing WelcomeMessage component for the JSX Excercise task */}
+      <WelcomeMessage />
     </div>
   );
 };
