8ddb752415188d752497ca53d93c90503d41c39c
unknown <amanyadav16544@gmail.com>
2025-06-17 17:20:19 +0530

    started learning styling in react , completed Inline styling part

---
+++ src/components/06.stylingInReact/InlineStyling.jsx
@@ -0,0 +1,14 @@
+// this is file for understanding how to apply inline styling to react component.
+// you can import this component to App component to check the result of this component.
+
+const InlineStyling = () => {
+  return (
+    <section>
+      <h1 style={{ color: "white", backgroundColor: "teal", padding: "2rem" }}>
+        Inline Styles
+      </h1>
+    </section>
+  );
+};
+
+export default InlineStyling;
