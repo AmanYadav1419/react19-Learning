990544c85fc03e405e6882aa007762c44f362662
unknown <amanyadav16544@gmail.com>
2025-06-17 17:49:40 +0530

    completed learning styling in react , completed external css file Styling part

---
+++ src/components/06.stylingInReact/ExternalCSSfile.jsx
@@ -0,0 +1,14 @@
+// this is file for understanding how to apply external styling using css file to react component.
+// you can import this component to App component to check the result of this component.
+
+import "./index.css";
+
+const ExternalCSSfile = () => {
+  return (
+    <section>
+      <h1>External CSS file to understand how to apply css externally.</h1>
+    </section>
+  );
+};
+
+export default ExternalCSSfile;
