2270c019b36ffbd9c36548072441ebbe7b262981
AmanYadav1419 <amanyadav16544@gmail.com>
2025-06-24 17:06:52 +0530

    reset factor

---
+++ src/components/07.StateAndHooks/UpdateArrayOfObjectsWithUseState.jsx
@@ -0,0 +1,17 @@
+// step 1 : import the useState hook from react to use it.
+import { useState } from "react";
+
+const UpdateArrayOfObjectsWithUseState = () => {
+    // step 2 : we are gonna use useState hook and initialised with array of object to it.
+    // and inside that we are gonna pass the array of objects.
+    useState([{
+        
+    }])
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+export default UpdateArrayOfObjectsWithUseState
