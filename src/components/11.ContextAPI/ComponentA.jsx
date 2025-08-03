ccfb44a5b8c0a1416838cd1f8a78857b866a73c3
unknown <amanyadav16544@gmail.com>
2025-08-03 14:08:06 +0530

    understand the next topic of more than one context , and data sending with code

--- src/components/11.ContextAPI/ComponentA.jsx
+++ src/components/11.ContextAPI/ComponentA.jsx
@@ -1,10 +1,22 @@
-import { Data } from "./ParentComponent";
-
+import { Data, Data1 } from "./ParentComponent";
 const ComponentA = () => {
   return (
+    // so to recive that data from ParentComponent we have to do like this.
     <Data.Consumer>
       {(name) => {
-        return <h1>My name is :- {name}</h1>;
+        // return <h1>My name is :- {name}</h1>;
+        return (
+          <Data1.Consumer>
+            {/* now insie that we have to pass the callback function again */}
+            {(age) => {
+              return (
+                <h1>
+                  My name is: {name} and I'm {age} years old.
+                </h1>
+              );
+            }}
+          </Data1.Consumer>
+        );
       }}
     </Data.Consumer>
   );
--- src/components/11.ContextAPI/ParentComponent.jsx
+++ src/components/11.ContextAPI/ParentComponent.jsx
@@ -1,14 +1,23 @@
 import { createContext } from "react";
 import ComponentA from "./ComponentA";
 
+// this is for the name data context
 export const Data = createContext();
+
+/** what if we had more than one context and data to send,
+ * here we are creating one more context to understand the concept. **/
+export const Data1 = createContext();
 
 const ParentComponent = () => {
   const name = "Aman Yadav From ContextAPI";
+  const age = 22;
 
   return (
-    <Data.Provider value={name}>
-      <ComponentA />
+      <Data.Provider value={name}>
+        {/* here we have another Context nested to the Context */}
+      <Data1.Provider value={age}>
+        <ComponentA />
+      </Data1.Provider>
     </Data.Provider>
   );
 };
