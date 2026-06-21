b303d4ba4c14a1578378b575e2e28566cd301a4f
Aman <amanyadav16544@gmail.com>
2026-06-21 11:26:01 +0530

    completed useId hook implmentation and usage of it

--- src/components/15.useId/useIdHookImplemenation.jsx
+++ src/components/15.useId/useIdHookImplemenation.jsx
@@ -1,7 +1,23 @@
+// now to use the uniqueid component in this component.
+import UniqueId from "./uniqueId";
 const useIdHookImplemenation = () => {
   return (
-    <div>useIdHookImplemenation</div>
-  )
-}
+    <div>
+      {/* importing the uniqueid component to use in the application*/}
+      <UniqueId />
 
-export default useIdHookImplemenation
+      {/* now we will use the paragraph tag and some random text */}
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga provident
+        placeat quos maiores sit cumque necessitatibus unde similique, magnam
+        quia numquam laboriosam accusamus earum itaque?
+      </p>
+
+      {/* and then again we will import our uniqueid component to use in the application. */}
+      <UniqueId />
+    </div>
+  );
+};
+
+// and then export the component to use in the application.
+export default useIdHookImplemenation;
