86eb77a7b27ecf12d4802045506a3af34271cb94
Aman <amanyadav16544@gmail.com>
2026-07-11 21:58:31 +0530

    fixed meals project bug and issues used axios  , added meals css file for better representation

--- src/01-BeginnerProjects/003-Mealsproject/Meals.jsx
+++ src/01-BeginnerProjects/003-Mealsproject/Meals.jsx
@@ -1,34 +1,40 @@
 // 003 Meals Project.
 
 // in this project we will fetch the data from meals db api with the help of useEffect.
-
+// with using axios package for data fetching , make sure to npm i axios before run.
+import axios from "axios";
 import { useState, useEffect } from "react";
+import "./meals.css";
 const Meals = () => {
   const [items, seItems] = useState([]);
 
-  // we are using fetch and .then method for the api calls and for the error catching we are using .catch method
+  // we are using axios package and .get method for the api calls and for the error catching we are using .catch method
   useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
-      .then((res) => res.json())
-      .then((res) => seItems(res.data.meals))
-      .catch((err) => console.error(err));
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        seItems(res.data.meals);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
   // itemsList function to render all the items in the ui.
   const itemsList = items.map(({ strMeal, strMealThumb, idMeal }) => {
     return (
-      <section key={idMeal}>
-        <img src={strMealThumb} alt={strMeal} />
-        <section>
-          <p>NAme :- {strMeal}</p>
-          <p>ID :- {idMeal}</p>
+      <section className="meals-card" key={idMeal}>
+        <img className="meals-img" src={strMealThumb} alt={strMeal} />
+        <section className="meals-content">
+          <p className="meals-p">Name :- {strMeal}</p>
+          <p className="meals-p">ID :- #{idMeal}</p>
         </section>
       </section>
     );
   });
 
   // we are rendering a function that will render the all the meals data in the ui.
-  return <div>{itemsList}</div>;
+  return <div className="meals-items-container">{itemsList}</div>;
 };
 
 export default Meals;
